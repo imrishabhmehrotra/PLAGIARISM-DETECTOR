382d2e1dcd75235e3691fcbc1199070125a9b7a6
Rishabh Mehrotra <rishabh.2024en1164@kiet.edu>
2023-12-13 13:22:29 +0530

    Plagiarism_detector

---
+++ main.py
@@ -0,0 +1,10 @@
+from difflib import SequenceMatcher
+
+#Using File Handling
+with open('demo1.txt') as one_file, open('demo2.txt') as two_file:
+    
+    data_file1 = one_file.read()
+    data_file2 = two_file.read()
+    
+    matches = SequenceMatcher(None, data_file1, data_file2).ratio()
+    print(f" The plagiarized content is {matches*100}% ")
